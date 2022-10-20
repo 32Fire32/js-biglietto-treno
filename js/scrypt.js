c7fe063ab3868af1c4c00d1cd68d03c9b78ba675
32Fire32 <nicola.gianfelici@hotmail.it>
2022-10-20 15:21:16 +0200

    esercizio base completo

---
+++ js/scrypt.js
@@ -0,0 +1,25 @@
+'strict mode'
+
+// chiedere lunghezza del tragitto
+const traveLenght = Number(prompt("Inserisci la lunghezza del tragitto in Km"));
+//chiedere età
+const userAge = Number(prompt("Inserisci la tua età"));
+//prezzo totale del viaggio 0.21€ x km
+let totalPrice = traveLenght * 0.21;
+console.log(traveLenght, userAge, totalPrice);
+//se minorenni sconto del 20% se over 65 scontro del 40%
+if (userAge < 18) {
+    totalPriceYoung = totalPrice - ((totalPrice * 20)/100);
+    console.log(totalPriceYoung);
+    document.querySelector("h1").innerHTML = totalPriceYoung.toFixed(2); 
+} else if (userAge >= 65) {
+    totalPriceOld = totalPrice - ((totalPrice * 40)/100);
+    console.log(totalPriceOld);
+    document.querySelector("h1").innerHTML = totalPriceOld.toFixed(2); 
+} else {
+    //output 2 cifre decimali
+    document.querySelector("h1").innerHTML = totalPrice.toFixed(2); 
+}
+
+
+
