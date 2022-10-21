59711f82d76830c65fd69b1c40bf767945da8a6b
32Fire32 <nicola.gianfelici@hotmail.it>
2022-10-21 10:03:39 +0200

    migliora il codice dopo lezione

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,16 +1,16 @@
 'strict mode'
 
-function change(){
-    let testo = document.querySelector(".apologies");
-    testo.style.display = "block";
-    }
-    setTimeout("change();", 5000);
-
 function wait(){
     let testo = document.querySelector(".wait");
     testo.style.display = "none";
     }
     setTimeout("wait();", 5000);
+
+function change(){
+    let testo = document.querySelector(".apologies");
+    testo.style.display = "block";
+    }
+    setTimeout("change();", 5000);
 
 
 // chiedere lunghezza del tragitto
@@ -22,13 +22,13 @@ let totalPrice = traveLenght * 0.21;
 console.log(traveLenght, userAge, totalPrice);
 //se minorenni sconto del 20% se over 65 scontro del 40%
 if (userAge < 18) {
-    totalPriceYoung = totalPrice - ((totalPrice * 20)/100);
-    console.log(totalPriceYoung);
-    document.querySelector("span").innerHTML = totalPriceYoung.toFixed(2); 
+    totalPrice = totalPrice - totalPrice * 0.2;
+    console.log(totalPrice);
+    document.querySelector("span").innerHTML = totalPrice.toFixed(2); 
 } else if (userAge >= 65) {
-    totalPriceOld = totalPrice - ((totalPrice * 40)/100);
-    console.log(totalPriceOld);
-    document.querySelector("span").innerHTML = totalPriceOld.toFixed(2); 
+    totalPrice = totalPrice - totalPrice * 0.4;
+    console.log(totalPrice);
+    document.querySelector("span").innerHTML = totalPrice.toFixed(2); 
 } else {
     //output 2 cifre decimali
     document.querySelector("span").innerHTML = totalPrice.toFixed(2); 
